8e948931cc56334318da5faedf9d1c47f7ac85d2
Jere Valtanen <jerevaltanen@msn.com>
2016-01-26 10:50:47 +0200

    Updated

---
+++ Excercise 1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create tyre
+            Tyre tyre1 = new Tyre { Name = "Nokia", Model = "Hakkapeliitta", Size = "205R16" };
+            Tyre tyre2 = new Tyre { Name = "Pirelli", Model = "JokuPirelli", Size = "225R16" };
+
+
+
+            // Create a car
+            Vehicle car = new Vehicle { Name = "Dodge", Model = "Viper" };
+
+            car.AddTyre(tyre1);
+            car.AddTyre(tyre1);
+            car.AddTyre(tyre2);
+            car.AddTyre(tyre2);
+
+            Console.WriteLine(car.ToString());
+        }
+    }
+}
---
+++ Excercise 1/Vehicle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise_1
+{
+    class Vehicle
+    {
+        public string Name { get; set; }
+        public string Model { get; set; }
+
+        public List<Tyre> tyres { get; }
+
+        public Vehicle()
+        {
+            tyres = new List<Tyre>();
+        }
+
+        public void AddTyre(Tyre tyre)
+        {
+        tyres.Add(tyre);
+        }
+
+        public override string ToString()
+        {
+            string sVehicle = "Vehicle Name: " + Name + "\nModel : " + Model + "\nTyres:\n";
+            foreach(Tyre tyre in tyres)
+            {
+                sVehicle += tyre.ToString() + "\n";
+            }
+            return sVehicle;
+        }
+    }
+}
